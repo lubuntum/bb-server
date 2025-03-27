1583f87726b441990be37c1832c8ecc9c452c1b2
lubuntum <lubuntum@gmail.com>
2025-03-28 02:19:16 +0700

    Добавил публичную папку для ресурсов

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
+const path = require("path")
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
 
+//static folder
+app.use(express.static(path.join(__dirname, 'public')))
 //middlewares
 const authentificateToken = require("./middleware/authentificateToken")
 
