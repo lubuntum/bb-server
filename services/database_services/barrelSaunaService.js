28e458782c44dba709f9cea0ceb219a3af42b2f0
lubuntum <lubuntum@gmail.com>
2025-03-29 03:50:36 +0700

    Адаптировал значения из запроса при сохранении бочки

--- services/database_services/barrelSaunaService.js
+++ services/database_services/barrelSaunaService.js
@@ -8,20 +8,21 @@ const createBarrelSauna = async (barrel) => {
             "door_id, tank_id, window_id, base_id, total_price, area_id, tension_ring_id, oven_placement_id) " +
             "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             const values = [
-                barrel.materialId,
-                barrel.formId,
-                barrel.ovenTypeId,
-                barrel.colorId,
-                barrel.ovenAdditionId,
-                barrel.doorId,
-                barrel.tankId,
-                barrel.windowId,
-                barrel.baseId,
-                barrel.totalPrice,
-                barrel.areaId,
-                barrel.tensionRingId,
-                barrel.ovenPlacementId
+                barrel.material_id,
+                barrel.form_id,
+                barrel.oven_type_id,
+                barrel.color_id,
+                barrel.oven_addition_id,
+                barrel.door_id,
+                barrel.tank_id,
+                barrel.window_id,
+                barrel.base_id,
+                barrel.total_price,
+                barrel.area_id,
+                barrel.tension_ring_id,
+                barrel.oven_placement_id
             ];
+        console.log(values)
         db.run(sql, values, function(err) {
                 if (err) return reject({status: 500, message: err})
                 console.log(this.lastID)
